44545579b9acfe9e034a5e145087e2bb597ddd92
Shady <shady@shadyproject.net>
2014-02-19 21:26:17 -0800

    first shitty attempt at pinch dismiss

---
+++ Estimate Cards/Transitions/ECPinchDismissTransition.h
@@ -0,0 +1,13 @@
+//
+//  ECPinchDismissTransition.h
+//  Estimate Cards
+//
+//  Created by Christopher Martin on 2/19/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECPinchDismissTransition : UIPercentDrivenInteractiveTransition
+
+@end
